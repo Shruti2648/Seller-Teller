fabe310e62cb9b8c0f8e7a50b7c71c24d2118055
Yangyin Hu <yangyinhu@MacBook-Pro.local>
2019-06-12 15:36:35 -0400

    fix connection between home page and passport

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -2,6 +2,9 @@ var db = require("../models");
 
 module.exports = function(app) {
   app.get("/api/users", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
     db.User.findAll({
       include: [db.Post]
     }).then(function(dbUser) {
@@ -10,6 +13,9 @@ module.exports = function(app) {
   });
 
   app.get("/api/users/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
     db.User.findOne({
       where: {
         id: req.params.id
@@ -35,4 +41,5 @@ module.exports = function(app) {
       res.json(dbUser);
     });
   });
+
 };
