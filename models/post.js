6b6441fe163243e16b8dc3fbfaed5ee74ff01c07
Yangyin Hu <yangyinhu@MacBook-Pro.local>
2019-06-13 02:35:22 -0400

    working version

--- models/post.js
+++ models/post.js
@@ -1,28 +1,48 @@
 module.exports = function(sequelize, DataTypes) {
   var Post = sequelize.define("Post", {
-    title: {
+    storeName: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
       }
     },
-    body: {
+    storeCategory: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    sale: {
       type: DataTypes.TEXT,
       allowNull: false,
       len: [1]
+    },
+    storeCity: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    storeState: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    stopDate: {
+      type: DataTypes.DATE,
+      allowNull: true
     }
   });
 
-  Post.associate = function(models) {
-    // We're saying that a Post should belong to an User
-    // A Post can't be created without an User due to the foreign key constraint
-    Post.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
+  // Post.associate = function(models) {
+  // We're saying that a Post should belong to an Author
+  // A Post can't be created without an Author due to the foreign key constraint
+  //   Post.belongsTo(models.User, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
 
   return Post;
 };
